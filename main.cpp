07387f4bb0de3217707d6f9b1ae25f6b6565fbdc
Aluminum0 <nullaluminum@gmail.com>
2025-12-14 19:47:09 +0400

    I added the pop up menu when you hover over the search bar.

--- main.cpp
+++ main.cpp
@@ -22,11 +22,18 @@ int main() {
         ClearBackground(RAYWHITE);
         DrawText("Listless Search:", 10, 20, 20, GRAY);
         Rectangle search_box = {recx,recy,width,height};
+
         //Border for search box
         DrawRectangleLines(200, 20, 700, 30, BLACK);
-        if (IsKeyDown(MOUSE_BUTTON_LEFT)) {
-            CheckCollisionPointRec(mus, search_box);
+
+        //This bit here is to add the pop-up menu when you move your cursor on the search bar.
+        //It took me 5 hours to figure out why it wasn't working.
+        if (CheckCollisionPointRec(mus, search_box)){
+            DrawRectangle(200, 50, 700, 200, GRAY);
         }
+
+
+
         EndDrawing();
     }
 
